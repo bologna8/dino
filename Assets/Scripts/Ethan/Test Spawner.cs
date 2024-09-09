7d392231e8d4c6d004a4b9805229abb78331b84d
worthless-protoplasm <151409881+worthless-protoplasm@users.noreply.github.com>
2024-09-08 21:46:19 -0400

    Added Audio Prefabs

---
+++ Assets/Scripts/Ethan/Test Spawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSpawner : MonoBehaviour
+{
+    
+    public GameObject effectToSpawn;
+
+    public double spawnRate;
+
+    private double spawnTime = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTime += Time.deltaTime;
+        if(spawnTime >= spawnRate){
+            Instantiate(effectToSpawn);
+            spawnTime = 0;
+        }
+    }
+}
