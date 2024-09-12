33aba5a3359af7c8551830e34a7508f2bddd0ecc
worthless-protoplasm <151409881+worthless-protoplasm@users.noreply.github.com>
2024-09-12 18:30:22 -0400

    Finished basic implementation of footstep audio

--- Assets/Scripts/Audio/FootstepAudio.cs
+++ Assets/Scripts/Audio/FootstepAudio.cs
@@ -5,8 +5,18 @@ using AYellowpaper.SerializedCollections;
 
 public class FootstepAudio : MonoBehaviour
 {
+    [Header("References")]
     [SerializedDictionary("GroundType", "List")]
     public SerializedDictionary<GroundType.Type, List<GameObject>> FootstepLists;
+
+    public Movement playerMovement;
+
+    public DetectGround detectGround;
+
+    [Header("Properties")]
+    public double SpawnRate = 0.5;
+
+    private double timeAlive = 0;
 
     void Start()
     {
@@ -16,6 +26,26 @@ public class FootstepAudio : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(playerMovement.onGround && (playerMovement.moveInput >= 0.5 || playerMovement.moveInput <= -0.5)){
+            timeAlive += Time.deltaTime;
+        }
+
+        if(timeAlive >= SpawnRate){
+            GameObject g = GetFootstep(detectGround.standingOn);
+            PoolManager.Instance.Spawn(g, transform.position, Quaternion.identity);
+            timeAlive = 0;
+        }
+
+    }
+
+    GameObject GetFootstep(GroundType.Type gt){
+        List<GameObject> lg = FootstepLists[gt];
+        if(lg.Count > 0){
+            int rand = Random.Range(0, lg.Count);
+            return lg[rand];
+        }
+        
+        return null;            
         
     }
 }
