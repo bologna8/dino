51c3c0d00b9a1c7f1d5eb2db2f6b96dc54147792
worthless-protoplasm <151409881+worthless-protoplasm@users.noreply.github.com>
2024-09-14 19:15:18 -0400

    Created MusicPlayer Prefab
    This prefab is placed in the scene, and any music playing prefabs are placed beneath it

---
+++ Assets/Scripts/Audio/MusicPlayer.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicPlayer : MonoBehaviour
+{
+    [Header("References")]
+    public AudioSource Primary;
+    public List<AudioSource> Secondary;
+
+    public AudioMixer Mixer;
+    
+    private const string track1 = "T1Vol";
+    private const string track2 = "T2Vol";
+    private const string track3 = "T3Vol";
+    private const string track4 = "T4Vol";
+
+    private static readonly List<string> tracks = new List<string> {track1, track2, track3, track4};
+
+    
+    
+
+    [Header("Crossfade Properties")]
+    public bool doingCrossfade = false;
+    public enum Track {track1, track2, track3, track4}
+    public Track trackToBringOut = 0;
+
+    private double CrossfadeTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(AudioSource source in Secondary){
+            source.timeSamples = Primary.timeSamples;       
+        }
+
+        if(doingCrossfade){
+            Crossfade(tracks[(int)trackToBringOut], tracks, 3f);
+        }
+    }
+
+    void Crossfade(string volA, List<string> allVolumes, float fadeTime){  // Makes one track turn on, and all others turn off.
+        
+        if(CrossfadeTime > fadeTime){
+            doingCrossfade = false;
+            CrossfadeTime = 0;
+            return;   
+        }else{
+
+            CrossfadeTime += Time.deltaTime;
+            
+            float logValue = (float) CrossfadeTime / fadeTime;
+            float invlogValue = 1 - logValue;
+            if(invlogValue == 0){
+                invlogValue = 0.001f;
+            }
+
+            foreach(string str in allVolumes){
+                if(str == volA){
+                    Mixer.SetFloat(str, Mathf.Log10(logValue) * 26);
+                }else{
+                    float x; 
+                    Mixer.GetFloat(str, out x);
+                    if(x > Mathf.Log10(invlogValue) * 26){
+                        Mixer.SetFloat(str, Mathf.Log10(invlogValue) * 26);
+                    }
+                }
+            }
+
+        }
+
+        
+    }
+}
