b718bcc0f99293366887b2bed2d9ff085e4d8511
bologna8 <bologna8@msu.edu>
2024-10-23 23:40:48 -0400

    Lantern running out of fuel/ oil replacement implemented
    double click to refill lantern

--- Assets/Scripts/UI/LanternController.cs
+++ Assets/Scripts/UI/LanternController.cs
@@ -1,26 +1,37 @@
 using UnityEngine;
-using UnityEngine.InputSystem; 
+using UnityEngine.InputSystem;
 
 public class LanternController : MonoBehaviour
 {
-    public GameObject lantern; 
+    public GameObject lantern;
+    public Item oilItem; 
+    public float maxEnergy = 100f; 
+    public float currentEnergy; 
+    public float depletionRate = 5f; 
+    public float refillAmount = 50f; 
 
-    private bool isLanternActive = false;  
-    private bool isLanternInInventory = false; 
-    private Inventory inventory;  
-    public Item lanternItem;  
+    private bool isLanternActive = false;
+    private bool isLanternInInventory = false;
+    private Inventory inventory;
+
+    public Item lanternItem; 
+
+    private float lastClickTime = 0f; 
+    public float doubleClickTime = 0.3f; 
+
     void Start()
     {
         inventory = FindObjectOfType<Inventory>();
+        lantern.SetActive(false);
+        currentEnergy = maxEnergy;
 
-        lantern.SetActive(false); 
         if (inventory == null)
         {
             Debug.LogError("Inventory script not found in the scene!");
         }
         else
         {
-            inventory.onItemChange += UpdateLanternStatus;  
+            inventory.onItemChange += UpdateLanternStatus;
         }
     }
 
@@ -28,7 +39,7 @@ public class LanternController : MonoBehaviour
     {
         if (inventory != null)
         {
-            inventory.onItemChange -= UpdateLanternStatus;  
+            inventory.onItemChange -= UpdateLanternStatus;
         }
     }
 
@@ -38,22 +49,65 @@ public class LanternController : MonoBehaviour
         {
             if (Mouse.current.leftButton.wasPressedThisFrame)
             {
-                ToggleLantern();
+                HandleLanternClick();
             }
         }
+
+        
+        if (isLanternActive)
+        {
+            DepleteEnergy();
+        }
+    }
+
+    void HandleLanternClick()
+    {
+        float currentTime = Time.time;
+
+        
+        if (currentTime - lastClickTime <= doubleClickTime)
+        {
+            
+            RefillLantern(); //if lantern is double clicked and there is oil in the inventory, refill the lantern 
+        }
+        else
+        {
+            ToggleLantern(); 
+        }
+
+        lastClickTime = currentTime; 
     }
 
     void ToggleLantern()
     {
         if (isLanternActive)
         {
-            lantern.SetActive(false);  
-            isLanternActive = false;
+            lantern.SetActive(false); 
+            isLanternActive = false; 
         }
         else
         {
-            lantern.SetActive(true); 
-            isLanternActive = true;
+            if (currentEnergy > 0) 
+            {
+                lantern.SetActive(true);
+                isLanternActive = true; 
+            }
+            else
+            {
+                Debug.Log("Not enough energy to turn on the lantern!");
+            }
+        }
+    }
+
+    void DepleteEnergy()
+    {
+        currentEnergy -= depletionRate * Time.deltaTime;
+        if (currentEnergy <= 0)
+        {
+            currentEnergy = 0; 
+            lantern.SetActive(false); 
+            isLanternActive = false; 
+            Debug.Log("Lantern is out of energy!");
         }
     }
 
@@ -64,7 +118,22 @@ public class LanternController : MonoBehaviour
         if (!isLanternInInventory)
         {
             lantern.SetActive(false); 
-            isLanternActive = false;
+            isLanternActive = false; 
+        }
+    }
+
+    public void RefillLantern()
+    {
+        if (inventory.ContainsItem(oilItem, 1)) 
+        {
+            currentEnergy += refillAmount; 
+            currentEnergy = Mathf.Min(currentEnergy, maxEnergy); 
+            inventory.RemoveItem(oilItem); 
+            Debug.Log("Lantern refilled! Current energy: " + currentEnergy);
+        }
+        else
+        {
+            Debug.Log("Not enough oil to refill the lantern!");
         }
     }
 }
