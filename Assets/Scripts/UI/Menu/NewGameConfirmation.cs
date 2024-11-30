c57ed384c91085fc5f51b3f32d0f5803f2988695
bologna8 <bologna8@msu.edu>
2024-11-29 19:36:10 -0500

    Main Menu Controller Functionality

--- Assets/Scripts/UI/Menu/NewGameConfirmation.cs
+++ Assets/Scripts/UI/Menu/NewGameConfirmation.cs
@@ -1,13 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class NewGameConfirmation : MonoBehaviour
 {
     [SerializeField] private GameObject confirmationPanel;
+    [SerializeField] private GameObject firstButton; 
+    [SerializeField] private GameObject mainMenuFirstButton; 
+
+    [SerializeField] private GameObject[] menuButtons; 
+
+    private GameObject previousSelectedButton; 
 
     public void ShowConfirmationPanel()
     {
+        previousSelectedButton = EventSystem.current.currentSelectedGameObject;
+
         confirmationPanel.SetActive(true);
+
+        SetMenuButtonsInteractable(false);
+
+        EventSystem.current.SetSelectedGameObject(firstButton);
     }
 
     public void StartNewGame()
@@ -18,5 +32,27 @@ public class NewGameConfirmation : MonoBehaviour
     public void CancelNewGame()
     {
         confirmationPanel.SetActive(false);
+
+        SetMenuButtonsInteractable(true);
+
+        if (previousSelectedButton != null)
+        {
+            EventSystem.current.SetSelectedGameObject(previousSelectedButton);
+        }
+        else
+        {
+            EventSystem.current.SetSelectedGameObject(mainMenuFirstButton);
+        }
+    }
+
+    private void SetMenuButtonsInteractable(bool state)
+    {
+        foreach (GameObject button in menuButtons)
+        {
+            if (button.TryGetComponent<Button>(out var btn))
+            {
+                btn.interactable = state;
+            }
+        }
     }
 }
